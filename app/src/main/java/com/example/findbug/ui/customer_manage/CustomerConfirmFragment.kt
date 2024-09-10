d34ab1aba4158d92c1d4a44e35c670cce17afb59
김성민 <arcn1345@gmail.com>
2024-09-10 23:53:46 +0900

    feat: CustomerConfirmFragment 버튼 클릭 이벤트 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/CustomerConfirmFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerConfirmFragment.kt
@@ -14,8 +14,15 @@ class CustomerConfirmFragment : BaseFragment<FragmentCustomerConfirmBinding
         >(R.layout.fragment_customer_confirm) {
 
     override fun setLayout() {
+        initButton()
+    }
+
+    private fun initButton() {
+
+        // 저장하기 버튼 누르면 방문완료 버튼 보여짐 및 저장하기 버튼 안보임
         binding.fragmentCustomerConfirmSaveBtn.setOnClickListener {
-            Toast.makeText(requireContext(), "저장", Toast.LENGTH_SHORT).show()
+            binding.fragmentCustomerConfirmVisitTv.visibility = View.VISIBLE
+            binding.fragmentCustomerConfirmSaveBtn.visibility = View.INVISIBLE
         }
     }
 
