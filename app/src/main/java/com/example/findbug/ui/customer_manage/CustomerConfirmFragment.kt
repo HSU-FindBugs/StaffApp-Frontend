bc15696a6ef7ecdca37d86dc0035e2ee993820d7
Seongmin <arcn1345@gmail.com>
2024-09-10 19:26:59 +0900

    feat: CustomerConfirmFragment BaseFragment로 변경

--- app/src/main/java/com/example/findbug/ui/customer_manage/CustomerConfirmFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerConfirmFragment.kt
@@ -5,21 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentCustomerConfirmBinding
 
-class CustomerConfirmFragment : Fragment() {
+class CustomerConfirmFragment : BaseFragment<FragmentCustomerConfirmBinding
+        >(R.layout.fragment_customer_confirm) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-
-        return inflater.inflate(R.layout.fragment_customer_confirm, container, false)
+    override fun setLayout() {
+        binding.fragmentCustomerConfirmSaveBtn.setOnClickListener {
+            Toast.makeText(requireContext(), "저장", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
