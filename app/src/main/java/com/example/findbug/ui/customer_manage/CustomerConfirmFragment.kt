24cd1656a1c66b99aebe214de973a7d694c38550
김성민 <arcn1345@gmail.com>
2024-09-11 18:09:44 +0900

    feat: 고객 확인 프래그먼트 버튼 클릭 리스너 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/CustomerConfirmFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerConfirmFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.findbug.R
 import com.example.findbug.base.BaseFragment
 import com.example.findbug.databinding.FragmentCustomerConfirmBinding
+import com.example.findbug.utils.extension.navigateSafe
 
 class CustomerConfirmFragment : BaseFragment<FragmentCustomerConfirmBinding
         >(R.layout.fragment_customer_confirm) {
@@ -24,6 +26,13 @@ class CustomerConfirmFragment : BaseFragment<FragmentCustomerConfirmBinding
             binding.fragmentCustomerConfirmVisitTv.visibility = View.VISIBLE
             binding.fragmentCustomerConfirmSaveBtn.visibility = View.INVISIBLE
         }
+
+        // 연필 모양 버튼 누르면 고객 특이사항 작성 페이지로 이동
+        binding.fragmentCustomerConfirmEditSignificantIb.setOnClickListener {
+            val action = CustomerConfirmFragmentDirections.actionCustomerConfirmFragmentToCustomerReportFragment()
+            findNavController().navigateSafe(action.actionId)
+        }
+
     }
 
 }
