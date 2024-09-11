54d9d7f9a283bba4d800648923fb2c123d18e628
김성민 <arcn1345@gmail.com>
2024-09-11 18:10:11 +0900

    feat: 고객 특이사항 작성 프래그먼트 버튼 및 스테이터스바 설정 함수 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/CustomerReportFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerReportFragment.kt
@@ -1,25 +1,36 @@
 package com.example.findbug.ui.customer_manage
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentCustomerReportBinding
 
-class CustomerReportFragment : Fragment() {
+class CustomerReportFragment : BaseFragment<FragmentCustomerReportBinding>(R.layout.fragment_customer_report) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onPause() {
+        super.onPause()
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.BG_Blue200)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_report, container, false)
+    override fun setLayout() {
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
+        initSettings()
+    }
+
+    private fun initSettings() {
+        initButton()
+    }
+
+    private fun initButton() {
+        binding.fragmentCustomerReportSaveBtn.setOnClickListener {
+            Log.d("로그","로그")
+        }
     }
 
 }
