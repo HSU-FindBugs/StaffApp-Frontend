3a4fc9a46cc793723fe9ec908150460e84331199
김성민 <arcn1345@gmail.com>
2024-09-11 17:22:50 +0900

    feat: 고객 등록/수정 프래그먼트 스피너 설정 함수, 버튼 설정 함수 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerUpsertFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerUpsertFragment.kt
@@ -1,25 +1,50 @@
 package com.example.findbug.ui.customer_manage.vp
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.navigation.fragment.findNavController
 import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentCustomerHomeBinding
+import com.example.findbug.databinding.FragmentCustomerUpsertBinding
+import com.example.findbug.ui.customer_manage.CustomerHomeFragmentDirections
+import com.example.findbug.utils.OptionSpinnerAdapter
 
-class CustomerUpsertFragment : Fragment() {
+class CustomerUpsertFragment :
+    BaseFragment<FragmentCustomerUpsertBinding>(R.layout.fragment_customer_upsert) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun setLayout() {
+        initSettings()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_upsert, container, false)
+    private fun initSettings() {
+        initSpinner()
+        initButton()
+    }
+
+    private fun initButton() {
+        binding.fragmentCustomerUpsertSaveBtn.setOnClickListener {
+            val action = CustomerHomeFragmentDirections.actionCustomerHomeFragmentToCustomerConfirmFragment()
+            findNavController().navigate(action.actionId)
+        }
+    }
+
+    private fun initSpinner() {
+        val spinnerList = resources.getStringArray(R.array.spinnerList).toList()
+        val adapter = OptionSpinnerAdapter(requireContext(),android.R.layout.simple_spinner_item, spinnerList)
+        binding.fragmentCustomerUpsertSpinner.adapter = adapter
+
+        binding.fragmentCustomerUpsertSpinner.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    if (!binding.fragmentCustomerUpsertSpinner.getItemIdAtPosition(position).equals("고객님의 멤버십 정보 선택")) {
+
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
     }
 
 }
