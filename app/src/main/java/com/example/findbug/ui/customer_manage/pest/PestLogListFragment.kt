f514b2b25be34409f9bd6bc978076c2c9b61bdc0
김성민 <arcn1345@gmail.com>
2024-09-14 16:00:06 +0900

    reactor: 프래그먼트 이름 변경

--- app/src/main/java/com/example/findbug/ui/customer_manage/pest/PestLogListFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/pest/PestLogListFragment.kt
@@ -1,16 +1,11 @@
 package com.example.findbug.ui.customer_manage.pest
 
-import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.findbug.R
 import com.example.findbug.base.BaseFragment
 import com.example.findbug.databinding.FragmentPestLogBinding
 
-class PestLogFragment : BaseFragment<FragmentPestLogBinding>(R.layout.fragment_pest_log) {
+class PestLogListFragment : BaseFragment<FragmentPestLogBinding>(R.layout.fragment_pest_log) {
 
     override fun setLayout() {
         Log.d("로그","로그")
