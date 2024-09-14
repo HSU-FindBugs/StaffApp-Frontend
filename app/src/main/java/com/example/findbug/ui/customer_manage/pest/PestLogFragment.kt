e06dd492c7235395a604a844b7d7e9f2007a2b53
김성민 <arcn1345@gmail.com>
2024-09-15 01:30:34 +0900

    feat: 해충 기록 화면 임시 버튼 코드 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/pest/PestLogFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/pest/PestLogFragment.kt
@@ -1,25 +1,31 @@
 package com.example.findbug.ui.customer_manage.pest
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentPestLogBinding
 
-class PestLogFragment : Fragment() {
+class PestLogFragment : BaseFragment<FragmentPestLogBinding>(R.layout.fragment_pest_log) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun setLayout() {
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.Blue300)
+        initSettings()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pest_log, container, false)
+    private fun initSettings() {
+        initButton()
+    }
+
+    private fun initButton() {
+        binding.fragmentPestLogPestShowVideoBtn.setOnClickListener {
+            Log.d("로그", "로그")
+        }
     }
 
 }
