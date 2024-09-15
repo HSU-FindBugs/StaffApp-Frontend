65b5eac65ef868afa151db4660c02f95036b2bd1
김성민 <arcn1345@gmail.com>
2024-09-15 21:04:09 +0900

    feat: 고객 확인 화면 버튼 클릭 리스너 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/CustomerConfirmFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerConfirmFragment.kt
@@ -22,20 +22,41 @@ class CustomerConfirmFragment : BaseFragment<FragmentCustomerConfirmBinding
     private fun initButton() {
 
         // 저장하기 버튼 누르면 방문완료 버튼 보여짐 및 저장하기 버튼 안보임
-        binding.fragmentCustomerConfirmSaveBtn.setOnClickListener {
-            binding.fragmentCustomerConfirmVisitTv.visibility = View.VISIBLE
-            binding.fragmentCustomerConfirmSaveBtn.visibility = View.INVISIBLE
-        }
+        with(binding) {
+            fragmentCustomerConfirmSaveBtn.setOnClickListener {
+                binding.fragmentCustomerConfirmVisitTv.visibility = View.VISIBLE
+                binding.fragmentCustomerConfirmSaveBtn.visibility = View.INVISIBLE
+            }
 
-        // 연필 모양 버튼 누르면 고객 특이사항 작성 페이지로 이동
-        binding.fragmentCustomerConfirmEditSignificantIb.setOnClickListener {
-            val action = CustomerConfirmFragmentDirections.actionCustomerConfirmFragmentToCustomerReportFragment()
-            findNavController().navigateSafe(action.actionId)
-        }
+            // 연필 모양 버튼 누르면 고객 특이사항 작성 페이지로 이동 (작성 및 수정)
+            fragmentCustomerConfirmEditSignificantIb.setOnClickListener {
+                val action =
+                    CustomerConfirmFragmentDirections.actionCustomerConfirmFragmentToCustomerReportFragment()
+                findNavController().navigateSafe(action.actionId)
+            }
 
-        binding.fragmentCustomerConfirmBugSheetIb.setOnClickListener {
-            val action = CustomerConfirmFragmentDirections.actionCustomerConfirmFragmentToPestLogFragment()
-            findNavController().navigateSafe(action.actionId)
+            // 해충 기록 화면으로 이동하는 버튼 (임시버튼)
+            fragmentCustomerConfirmBugSheetIb.setOnClickListener {
+                val action =
+                    CustomerConfirmFragmentDirections.actionCustomerConfirmFragmentToPestLogFragment()
+                findNavController().navigateSafe(action.actionId)
+            }
+
+            // 감지 영상 목록 화면으로 이동하는 버튼
+            fragmentCustomerConfirmVideoListIb.setOnClickListener {
+                val action = CustomerConfirmFragmentDirections.actionCustomerConfirmFragmentToDetectionVideoListFragment()
+                findNavController().navigateSafe(action.actionId)
+            }
+
+            // 실시간 영상 보기 화면으로 이동하는 버튼
+            fragmentCustomerConfirmLiveIb.setOnClickListener {
+
+            }
+
+            // 카메라 추가 화면으로 이동하는 버튼
+            fragmentCustomerConfirmAddCameraIb.setOnClickListener {
+
+            }
         }
 
     }
