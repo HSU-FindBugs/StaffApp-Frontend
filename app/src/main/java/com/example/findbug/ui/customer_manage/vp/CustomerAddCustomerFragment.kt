feec3b7cb7d2c2ee4b22f3687b48170f5181a7b9
김성민 <arcn1345@gmail.com>
2024-09-16 00:09:37 +0900

    reactor: 고객 정보 수정 프래그먼트 xml 완성

--- app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerAddCustomerFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerAddCustomerFragment.kt
@@ -3,10 +3,10 @@ package com.example.findbug.ui.customer_manage.vp
 import androidx.navigation.fragment.findNavController
 import com.example.findbug.R
 import com.example.findbug.base.BaseFragment
-import com.example.findbug.databinding.FragmentCustomerUpsertBinding
+import com.example.findbug.databinding.FragmentAddCustomerBinding
 
 class CustomerAddCustomerFragment :
-    BaseFragment<FragmentCustomerUpsertBinding>(R.layout.fragment_add_customer) {
+    BaseFragment<FragmentAddCustomerBinding>(R.layout.fragment_add_customer) {
 
     override fun setLayout() {
         initSettings()
