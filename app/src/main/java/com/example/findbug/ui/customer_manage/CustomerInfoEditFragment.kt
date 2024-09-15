0da7254452de6ff15d1accac2c9cb5d4b3f7abb6
김성민 <arcn1345@gmail.com>
2024-09-15 23:58:27 +0900

    feat: 고객 정보 수정 프래그먼트 추가

---
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerInfoEditFragment.kt
@@ -0,0 +1,26 @@
+package com.example.findbug.ui.customer_manage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentCustomerInfoEditBinding
+
+class CustomerInfoEditFragment : BaseFragment<FragmentCustomerInfoEditBinding>(R.layout.fragment_customer_info_edit) {
+
+    override fun setLayout() {
+
+    }
+
+    private fun initSettings() {
+
+    }
+
+    private fun initButtons() {
+
+    }
+
+}
