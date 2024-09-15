63bc8ccb5685a359fe23b7f6d3ed1f2f76ce6cd9
김성민 <arcn1345@gmail.com>
2024-09-15 22:11:48 +0900

    feat: 홈 프래그먼트 배경색 흰색으로 변경

--- app/src/main/java/com/example/findbug/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/findbug/ui/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.example.findbug.R
 import com.example.findbug.base.BaseFragment
@@ -14,7 +15,13 @@ import com.example.findbug.utils.extension.navigateSafe
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
+    override fun onPause() {
+        super.onPause()
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.BG_Blue200)
+    }
+
     override fun setLayout() {
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
         initButton()
     }
 
