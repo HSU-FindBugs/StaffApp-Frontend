0351308a6ba678f208e5f0ff252f8ee0306267a6
김성민 <arcn1345@gmail.com>
2024-09-15 22:40:47 +0900

    feat: 카메라 등록 프래그먼트 스테이터스바 색 설정 코드 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/CustomerAddCameraFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerAddCameraFragment.kt
@@ -6,14 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.findbug.R
 import com.example.findbug.base.BaseFragment
 import com.example.findbug.databinding.FragmentCustomerAddCameraBinding
 
 class CustomerAddCameraFragment : BaseFragment<FragmentCustomerAddCameraBinding>(R.layout.fragment_customer_add_camera) {
 
-    override fun setLayout() {
+    override fun onPause() {
+        super.onPause()
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.BG_Blue200)
+    }
 
+    override fun setLayout() {
+        requireActivity().window.statusBarColor = ContextCompat.getColor(requireContext(), R.color.white)
     }
 
     private fun initSettings() {
