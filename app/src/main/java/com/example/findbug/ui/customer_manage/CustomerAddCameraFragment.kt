10e0986f63c519087d2341f43e63437836997b06
김성민 <arcn1345@gmail.com>
2024-09-15 21:42:46 +0900

    feat: 카메라 등록 프래그먼트 추가

---
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerAddCameraFragment.kt
@@ -0,0 +1,27 @@
+package com.example.findbug.ui.customer_manage
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentCustomerAddCameraBinding
+
+class CustomerAddCameraFragment : BaseFragment<FragmentCustomerAddCameraBinding>(R.layout.fragment_customer_add_camera) {
+
+    override fun setLayout() {
+
+    }
+
+    private fun initSettings() {
+        Log.d("로그", "로그")
+    }
+
+    private fun initEditText() {
+
+    }
+
+}
