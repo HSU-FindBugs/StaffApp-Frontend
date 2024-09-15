2d200c06375657ad88d7756c625174dd53bbd5c9
김성민 <arcn1345@gmail.com>
2024-09-16 00:01:18 +0900

    reactor: 고객 추가 프래그먼트 일부 수정

--- app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerUpsertFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerUpsertFragment.kt
@@ -1,14 +1,9 @@
 package com.example.findbug.ui.customer_manage.vp
 
-import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
 import com.example.findbug.R
 import com.example.findbug.base.BaseFragment
-import com.example.findbug.databinding.FragmentCustomerHomeBinding
 import com.example.findbug.databinding.FragmentCustomerUpsertBinding
-import com.example.findbug.ui.customer_manage.CustomerHomeFragmentDirections
 
 class CustomerUpsertFragment :
     BaseFragment<FragmentCustomerUpsertBinding>(R.layout.fragment_customer_upsert) {
