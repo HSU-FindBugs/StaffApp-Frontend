f72ff01fc95a609cff8f9c712bf11d8b59db3c45
김성민 <arcn1345@gmail.com>
2024-09-16 18:29:58 +0900

    feat: 고객찾기 프래그먼트에서 검색 프래그먼트로 이동 하는 버튼 클릭 리스너 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerFindFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerFindFragment.kt
@@ -5,20 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentCustomerFindBinding
+import com.example.findbug.utils.extension.navigateSafe
 
-class CustomerFindFragment : Fragment() {
+class CustomerFindFragment : BaseFragment<FragmentCustomerFindBinding>(R.layout.fragment_customer_find) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun setLayout() {
+        goSearchPage()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_find, container, false)
+    private fun initSettings() {
+        goSearchPage()
+    }
+
+    private fun goSearchPage() {
+
+        val action = CustomerHomeFragmentDirections.actionCustomerHomeFragmentToCustomerSearchFragment()
+        with(binding) {
+            fragmentCustomerFindSearchTv.setOnClickListener {
+                findNavController().navigateSafe(action.actionId)
+            }
+            fragmentCustomerFindSearchIb.setOnClickListener {
+                findNavController().navigateSafe(action.actionId)
+            }
+        }
     }
 
 }
