ad1abbbc956f9b1f4f78460279fc1e1e37ede7fd
김성민 <arcn1345@gmail.com>
2024-09-16 17:29:29 +0900

    feat: 고객 검색 프래그먼트 추가

---
+++ app/src/main/java/com/example/findbug/ui/customer_manage/CustomerSearchFragment.kt
@@ -0,0 +1,22 @@
+package com.example.findbug.ui.customer_manage
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentCustomerSearchBinding
+
+class CustomerSearchFragment : BaseFragment<FragmentCustomerSearchBinding>(R.layout.fragment_customer_search) {
+
+    override fun setLayout() {
+
+    }
+
+    private fun initSettings() {
+
+    }
+
+}
