dd72959bcb57aec4212ff3b2b68f9f2539f81fc1
김성민 <arcn1345@gmail.com>
2024-09-19 00:05:40 +0900

    feat: 공지사항 리사이클러뷰 아이템 xml 클릭리스너 데이터 추가

--- app/src/main/java/com/example/findbug/ui/home/NoticeListAdapter.kt
+++ app/src/main/java/com/example/findbug/ui/home/NoticeListAdapter.kt
@@ -4,20 +4,21 @@ import com.example.findbug.R
 import com.example.findbug.base.BaseAdapter
 import com.example.findbug.base.BaseDiffCallback
 import com.example.findbug.databinding.ItemCustomerListBinding
+import com.example.findbug.databinding.ItemNoticesBinding
 import com.example.findbug.utils.listener.RVClickListener
 
 // 홈 - 공지사항 리사이클러뷰 어탭터
-class NoticeListAdapter(private val clickListener: RVClickListener) : BaseAdapter<DummyTest, ItemCustomerListBinding>(
+class NoticeListAdapter(private val clickListener: RVClickListener) : BaseAdapter<DummyTest, ItemNoticesBinding>(
     BaseDiffCallback (
         itemTheSame = { oldItem, newItem -> oldItem == newItem },
         contentsTheSame =  { oldItem, newItem -> oldItem == newItem }
     )
 ) {
     override val layoutId: Int
-        get() = R.layout.item_customer_list
+        get() = R.layout.item_notices
 
-    override fun bind(binding: ItemCustomerListBinding, item: DummyTest) {
-
+    override fun bind(binding: ItemNoticesBinding, item: DummyTest) {
+        binding.clickListener = clickListener
         binding.root.setOnClickListener {
             clickListener.onItemClick(item)
         }
