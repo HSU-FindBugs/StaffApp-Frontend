4644f20e680f726aaa5a957f83fcf6531614d18f
김성민 <arcn1345@gmail.com>
2024-09-19 00:09:25 +0900

    reactor: 공지사항 목록 어댑터 오류 수정

--- app/src/main/java/com/example/findbug/ui/home/NoticeListAdapter.kt
+++ app/src/main/java/com/example/findbug/ui/home/NoticeListAdapter.kt
@@ -3,11 +3,11 @@ package com.example.findbug.ui.home
 import com.example.findbug.R
 import com.example.findbug.base.BaseAdapter
 import com.example.findbug.base.BaseDiffCallback
-import com.example.findbug.databinding.ItemNoticesBinding
+import com.example.findbug.databinding.ItemNoticeListBinding
 import com.example.findbug.utils.listener.RVClickListener
 
 // 홈 - 공지사항 리사이클러뷰 어탭터
-class NoticeListAdapter(private val clickListener: RVClickListener) : BaseAdapter<DummyTest, ItemNoticesBinding>(
+class NoticeListAdapter(private val clickListener: RVClickListener) : BaseAdapter<DummyTest, ItemNoticeListBinding>(
     BaseDiffCallback (
         itemTheSame = { oldItem, newItem -> oldItem == newItem },
         contentsTheSame =  { oldItem, newItem -> oldItem == newItem }
@@ -16,7 +16,7 @@ class NoticeListAdapter(private val clickListener: RVClickListener) : BaseAdapte
     override val layoutId: Int
         get() = R.layout.item_notice_list
 
-    override fun bind(binding: ItemNoticesBinding, item: DummyTest) {
+    override fun bind(binding: ItemNoticeListBinding, item: DummyTest) {
         binding.clickListener = clickListener
         binding.root.setOnClickListener {
             clickListener.onItemClick(item)
