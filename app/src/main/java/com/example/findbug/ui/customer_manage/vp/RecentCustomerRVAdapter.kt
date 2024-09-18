468e4800d6b1141a52c4e4605fcd1b98671cdaee
김성민 <arcn1345@gmail.com>
2024-09-19 01:07:24 +0900

    feat: 최근 검색 고객 리사이클러뷰 어댑터 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/vp/RecentCustomerRVAdapter.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/vp/RecentCustomerRVAdapter.kt
@@ -4,20 +4,21 @@ import com.example.findbug.R
 import com.example.findbug.base.BaseAdapter
 import com.example.findbug.base.BaseDiffCallback
 import com.example.findbug.databinding.ItemCustomerListBinding
+import com.example.findbug.databinding.ItemRecentSearchBinding
 import com.example.findbug.ui.home.DummyTest
 import com.example.findbug.utils.listener.RVClickListener
 
-// 고객 관리 - 홈 (탭1) - 고객 목록 리사이클러뷰 어댑터
-class CustomerListRVAdapter(private val clickListener: RVClickListener): BaseAdapter<DummyTest, ItemCustomerListBinding>(
+// 고객 관리(검색 화면) - 최근 검색한 고객 리사이클러뷰 어댑터
+class RecentCustomerRVAdapter(private val clickListener: RVClickListener): BaseAdapter<DummyTest, ItemRecentSearchBinding>(
     diffCallback = BaseDiffCallback(
         itemTheSame = { oldItem, newItem -> oldItem == newItem },
         contentsTheSame =  { oldItem, newItem -> oldItem == newItem }
     )
 ) {
     override val layoutId: Int
-        get() = R.layout.item_customer_list
+        get() = R.layout.item_recent_search
 
-    override fun bind(binding: ItemCustomerListBinding, item: DummyTest) {
+    override fun bind(binding: ItemRecentSearchBinding, item: DummyTest) {
 
         binding.clickListener = clickListener
         binding.root.setOnClickListener {
