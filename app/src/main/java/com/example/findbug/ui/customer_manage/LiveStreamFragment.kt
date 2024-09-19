4a2bb2469b941c21e27e23c7bdde813eef9878c4
김성민 <arcn1345@gmail.com>
2024-09-19 23:42:06 +0900

    feat: 실시간 영상 보기 프래그먼트 추가

---
+++ app/src/main/java/com/example/findbug/ui/customer_manage/LiveStreamFragment.kt
@@ -0,0 +1,13 @@
+package com.example.findbug.ui.customer_manage
+
+import com.example.findbug.R
+import com.example.findbug.base.BaseFragment
+import com.example.findbug.databinding.FragmentLiveStreamBinding
+
+class LiveStreamFragment : BaseFragment<FragmentLiveStreamBinding>(R.layout.fragment_live_stream) {
+
+    override fun setLayout() {
+        TODO("Not yet implemented")
+    }
+
+}
