560ddf89040052dd38257218495d7835299e25fd
김성민 <arcn1345@gmail.com>
2024-09-20 01:10:34 +0900

    reactor: 고객 등록 프래그먼트 코드 간소화

--- app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerAddCustomerFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/vp/CustomerAddCustomerFragment.kt
@@ -35,50 +35,69 @@ class CustomerAddCustomerFragment :
 
     // 버튼 활성화 함수
     private fun activateBtn() {
-        val saveBtn = binding.fragmentCustomerUpsertSaveBtn
-        val radioGroup = binding.fragmentCustomerUpsertRadioGroup
-        val buttonBackground = R.drawable.shape_rounded_rect_50dp
-        val enableButtonTextColor = ContextCompat.getColor(requireContext(), R.color.white)
-        val disableButtonTextColor = ContextCompat.getColor(requireContext(), R.color.Blue500)
+        with(binding) {
+            val saveBtn = fragmentCustomerUpsertSaveBtn
+            val radioGroup = fragmentCustomerUpsertRadioGroup
+            val buttonBackground = R.drawable.shape_rounded_rect_50dp
+            val enableButtonTextColor = ContextCompat.getColor(requireContext(), R.color.white)
+            val disableButtonTextColor = ContextCompat.getColor(requireContext(), R.color.Blue500)
 
-        // 버튼 활성화 여부를 조건 처리하는 로컬 함수
-        fun updateSaveButtonState() {
-            val nameEt = binding.fragmentCustomerUpsertNameEt.text.toString()
-            val phoneEt = binding.fragmentCustomerUpsertPhoneEt.text.toString()
-            val addressEt = binding.fragmentCustomerUpsertAddressEt.text.toString()
-            val detailAddressEt = binding.fragmentCustomerUpsertDetailAddressEt.text.toString()
-            val isRadioButtonSelected = radioGroup.checkedRadioButtonId != -1
+            // 버튼 활성화 여부를 조건 처리하는 로컬 함수
+            fun updateSaveButtonState() {
+                val nameEt = fragmentCustomerUpsertNameEt.text.toString()
+                val phoneEt = fragmentCustomerUpsertPhoneEt.text.toString()
+                val addressEt = fragmentCustomerUpsertAddressEt.text.toString()
+                val detailAddressEt = fragmentCustomerUpsertDetailAddressEt.text.toString()
+                val isRadioButtonSelected = radioGroup.checkedRadioButtonId != -1
 
-            if (nameEt.isNotEmpty() && phoneEt.isNotEmpty() && addressEt.isNotEmpty() && detailAddressEt.isNotEmpty() && isRadioButtonSelected) {
-                saveBtn.setBackgroundResource(buttonBackground)
-                saveBtn.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.Blue700))
-                saveBtn.setTextColor(enableButtonTextColor)
-                saveBtn.isEnabled = true
-            } else {
-                saveBtn.setBackgroundResource(buttonBackground)
-                saveBtn.setBackgroundTintList(ContextCompat.getColorStateList(requireContext(), R.color.Blue300))
-                saveBtn.setTextColor(disableButtonTextColor)
-                saveBtn.isEnabled = false
+                if (nameEt.isNotEmpty() && phoneEt.isNotEmpty() && addressEt.isNotEmpty() && detailAddressEt.isNotEmpty() && isRadioButtonSelected) {
+                    saveBtn.setBackgroundResource(buttonBackground)
+                    saveBtn.setBackgroundTintList(
+                        ContextCompat.getColorStateList(
+                            requireContext(),
+                            R.color.Blue700
+                        )
+                    )
+                    saveBtn.setTextColor(enableButtonTextColor)
+                    saveBtn.isEnabled = true
+                } else {
+                    saveBtn.setBackgroundResource(buttonBackground)
+                    saveBtn.setBackgroundTintList(
+                        ContextCompat.getColorStateList(
+                            requireContext(),
+                            R.color.Blue300
+                        )
+                    )
+                    saveBtn.setTextColor(disableButtonTextColor)
+                    saveBtn.isEnabled = false
+                }
             }
-        }
 
-        val textWatcher = object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
-            override fun afterTextChanged(s: Editable?) {
-                updateSaveButtonState() // 텍스트 변경 시 버튼 상태 업데이트
+            val textWatcher = object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+                override fun afterTextChanged(s: Editable?) {
+                    updateSaveButtonState() // 텍스트 변경 시 버튼 상태 업데이트
+                }
             }
-        }
 
-        // EditText에 TextWatcher 추가
-        binding.fragmentCustomerUpsertNameEt.addTextChangedListener(textWatcher)
-        binding.fragmentCustomerUpsertPhoneEt.addTextChangedListener(textWatcher)
-        binding.fragmentCustomerUpsertAddressEt.addTextChangedListener(textWatcher)
-        binding.fragmentCustomerUpsertDetailAddressEt.addTextChangedListener(textWatcher)
+            // EditText에 TextWatcher 추가
+            fragmentCustomerUpsertNameEt.addTextChangedListener(textWatcher)
+            fragmentCustomerUpsertPhoneEt.addTextChangedListener(textWatcher)
+            fragmentCustomerUpsertAddressEt.addTextChangedListener(textWatcher)
+            fragmentCustomerUpsertDetailAddressEt.addTextChangedListener(textWatcher)
 
-        // RadioGroup에 선택 변경 리스너 추가
-        radioGroup.setOnCheckedChangeListener { _, _ ->
-            updateSaveButtonState() // 라디오 버튼 선택할 때 마다 버튼 상태 업데이트
+            // RadioGroup에 선택 변경 리스너 추가
+            radioGroup.setOnCheckedChangeListener { _, _ ->
+                updateSaveButtonState() // 라디오 버튼 선택할 때 마다 버튼 상태 업데이트
+            }
         }
     }
 
