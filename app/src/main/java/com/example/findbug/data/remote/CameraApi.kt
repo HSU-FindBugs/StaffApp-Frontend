8d2361893df6653dce3febae4d2343166ae6d272
김성민 <arcn1345@gmail.com>
2024-09-21 00:08:59 +0900

    feat: Api 인터페이스 추가

---
+++ app/src/main/java/com/example/findbug/data/remote/CameraApi.kt
@@ -0,0 +1,4 @@
+package com.example.findbug.data.remote
+
+interface CameraApi {
+}
---
+++ app/src/main/java/com/example/findbug/data/remote/CustomerApi.kt
@@ -0,0 +1,8 @@
+package com.example.findbug.data.remote
+
+interface CustomerApi {
+
+    // 고객 관리 - 고객 찾기
+
+    // 고객 관리 - 고객 정보 확인
+}
