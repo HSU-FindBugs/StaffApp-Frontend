75d5401b0ee4cc6dd43d2369d35a3c4a3b647adc
김성민 <arcn1345@gmail.com>
2024-09-21 01:14:39 +0900

    feat: 카메라 api 추가

--- app/src/main/java/com/example/findbug/data/remote/CameraApi.kt
+++ app/src/main/java/com/example/findbug/data/remote/CameraApi.kt
@@ -1,4 +1,27 @@
 package com.example.findbug.data.remote
 
+import com.example.findbug.base.BaseListResponse
+import com.example.findbug.base.BaseResponse
+import com.example.findbug.domain.model.request.CameraRegisterRequestDto
+import com.example.findbug.domain.model.response.CameraListResponse
+import com.example.findbug.domain.model.response.CameraSaveResponse
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface CameraApi {
+
+    // 카메라 등록
+    @POST("/camera")
+    suspend fun addCamera(
+        @Body cameraRegisterRequestDto: CameraRegisterRequestDto
+    ): BaseResponse<CameraSaveResponse>
+
+    // 카메라 조회
+    @GET("/camera/{member_id}")
+    suspend fun getCamera(
+        @Path("member_id") member_id: Int
+    ) : BaseListResponse<CameraListResponse>
+
 }
