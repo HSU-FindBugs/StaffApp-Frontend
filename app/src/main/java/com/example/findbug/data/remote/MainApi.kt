5ce2f4189910501690fdbe204c861532c353bcc6
김성민 <arcn1345@gmail.com>
2024-09-21 01:24:59 +0900

    reactor: MainApi 수정

--- app/src/main/java/com/example/findbug/data/remote/MainApi.kt
+++ app/src/main/java/com/example/findbug/data/remote/MainApi.kt
@@ -3,6 +3,7 @@ package com.example.findbug.data.remote
 import com.example.findbug.base.BaseResponse
 import com.example.findbug.domain.model.request.CameraRegisterRequestDto
 import com.example.findbug.domain.model.response.CameraSaveResponse
+import com.example.findbug.domain.model.response.MainPageResponse
 import com.example.findbug.domain.model.response.SseEmitter
 import retrofit2.http.Body
 import retrofit2.http.GET
@@ -12,10 +13,10 @@ import retrofit2.http.Path
 interface MainApi {
 
     // 메인 페이지 조회
-    @POST("/camera")
+    @POST("/api/main/{id}")
     suspend fun addCamera(
-        @Body cameraRegisterRequestDto: CameraRegisterRequestDto
-    ): BaseResponse<CameraSaveResponse>
+        @Path("id") id: Int
+    ): BaseResponse<MainPageResponse>
 
     // sse-controller
     @GET("/detection-events/connect/{staffId}")
