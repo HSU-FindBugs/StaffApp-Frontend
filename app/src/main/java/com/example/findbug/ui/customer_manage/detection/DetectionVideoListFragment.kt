5d06657a802cf1e9590108914327cbfbe371fb18
김성민 <arcn1345@gmail.com>
2024-09-23 03:42:57 +0900

    feat: 감지 영상 목록 프래그먼트 API 연결 및 데이터 바인딩

--- app/src/main/java/com/example/findbug/ui/customer_manage/detection/DetectionVideoListFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/detection/DetectionVideoListFragment.kt
@@ -1,27 +1,58 @@
 package com.example.findbug.ui.customer_manage.detection
 
-import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.findbug.R
 import com.example.findbug.base.BaseFragment
 import com.example.findbug.databinding.FragmentDetectionVideoListBinding
+import com.example.findbug.ui.customer_manage.CustomerViewModel
+import com.example.findbug.utils.listener.RVClickListener
+import kotlinx.coroutines.launch
 
-class DetectionVideoListFragment : BaseFragment<FragmentDetectionVideoListBinding>(R.layout.fragment_detection_video_list) {
+class DetectionVideoListFragment : BaseFragment<FragmentDetectionVideoListBinding>(R.layout.fragment_detection_video_list), RVClickListener {
+
+    private val customerViewModel : CustomerViewModel by activityViewModels()
+    private var memberId: Long = 0
+
+    private lateinit var detectionVideoListRVAdapter: DetectionVideoListRVAdapter
 
     override fun setLayout() {
         initSettings()
+        getPostId()
+        observeViewModel()
+        initAdapter()
     }
 
     private fun initSettings() {
         setToolbarNavigation(binding.fragmentDetectionVideoListToolbar.toolbarPreviousIb)
     }
 
-    private fun initAdapter() {
+    private fun getPostId() {
+        arguments?.let {
+            memberId = it.getLong("memberId")?.toLong() ?: 1
+        }
+    }
 
+    private fun initAdapter() {
+        detectionVideoListRVAdapter = DetectionVideoListRVAdapter(this)
+        binding.fragmentDetectionVideoListRv.adapter = detectionVideoListRVAdapter
+    }
+
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                customerViewModel.getPestLog(1, memberId)
+                customerViewModel.detectionHistoryResponse.collect() { res ->
+                    detectionVideoListRVAdapter.submitList(res.body()?.detectionHistoryDtoList)
+                }
+            }
+        }
+    }
+
+    override fun onItemClick(item: Any) {
+        TODO("Not yet implemented")
     }
 
 }
