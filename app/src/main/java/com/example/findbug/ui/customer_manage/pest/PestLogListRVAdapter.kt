8398e954fcd1bd1fffb1e2bd1e7f7b6ccb394710
김성민 <arcn1345@gmail.com>
2024-09-23 03:42:41 +0900

    feat: 해충 기록 목록 프래그먼트 API 연결 및 데이터 바인딩

--- app/src/main/java/com/example/findbug/ui/customer_manage/pest/PestLogListRVAdapter.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/pest/PestLogListRVAdapter.kt
@@ -4,11 +4,12 @@ import com.example.findbug.R
 import com.example.findbug.base.BaseAdapter
 import com.example.findbug.base.BaseDiffCallback
 import com.example.findbug.databinding.ItemPestListBinding
+import com.example.findbug.domain.model.response.DetectionHistory
 import com.example.findbug.ui.home.DummyTest
 import com.example.findbug.utils.listener.RVClickListener
 
 // 고객 관리 - 해충기록 리사이클러뷰 어댑터
-class PestLogListRVAdapter(private val clickListener: RVClickListener) : BaseAdapter<DummyTest, ItemPestListBinding>(
+class PestLogListRVAdapter(private val clickListener: RVClickListener) : BaseAdapter<DetectionHistory, ItemPestListBinding>(
     BaseDiffCallback (
         itemTheSame = { oldItem, newItem -> oldItem == newItem },
         contentsTheSame =  { oldItem, newItem -> oldItem == newItem }
@@ -17,7 +18,8 @@ class PestLogListRVAdapter(private val clickListener: RVClickListener) : BaseAda
     override val layoutId: Int
         get() = R.layout.fragment_pest_log_list
 
-    override fun bind(binding: ItemPestListBinding, item: DummyTest) {
+    override fun bind(binding: ItemPestListBinding, item: DetectionHistory) {
+        binding.detectionHistory = item
         binding.clickListener = clickListener
         binding.root.setOnClickListener {
             clickListener.onItemClick(item)
