4e023c2be0870aebcc77e41f85386c5f2df58059
김성민 <arcn1345@gmail.com>
2024-09-24 02:45:56 +0900

    feat: 해충 기록 목록 리사이클러뷰 클릭 리스너 코드 추가

--- app/src/main/java/com/example/findbug/ui/customer_manage/pest/PestLogListFragment.kt
+++ app/src/main/java/com/example/findbug/ui/customer_manage/pest/PestLogListFragment.kt
@@ -1,23 +1,29 @@
 package com.example.findbug.ui.customer_manage.pest
 
+import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
 import com.example.findbug.R
 import com.example.findbug.base.BaseFragment
 import com.example.findbug.databinding.FragmentPestLogBinding
 import com.example.findbug.databinding.FragmentPestLogListBinding
+import com.example.findbug.domain.model.response.DetectionHistory
 import com.example.findbug.ui.customer_manage.CustomerViewModel
+import com.example.findbug.ui.home.MainViewModel
+import com.example.findbug.utils.extension.navigateSafe
 import com.example.findbug.utils.listener.RVClickListener
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class PestLogListFragment : BaseFragment<FragmentPestLogListBinding>(R.layout.fragment_pest_log_list), RVClickListener {
 
-    private val customerViewModel : CustomerViewModel by activityViewModels()
+    private val mainViewModel : MainViewModel by activityViewModels()
     private var memberId: Long = 0
+    private var detectionHistoryId: Long = 0
 
     private lateinit var pestLogListRVAdapter: PestLogListRVAdapter
 
@@ -28,8 +34,8 @@ class PestLogListFragment : BaseFragment<FragmentPestLogListBinding>(R.layout.fr
     private fun initSettings() {
         setToolbarNavigation(binding.fragmentPestLogListToolbar.toolbarPreviousIb)
         getPostId()
-        observeViewModel()
         initAdapter()
+        observeViewModel()
     }
 
     private fun getPostId() {
@@ -46,16 +52,22 @@ class PestLogListFragment : BaseFragment<FragmentPestLogListBinding>(R.layout.fr
     private fun observeViewModel() {
         lifecycleScope.launch {
             repeatOnLifecycle(Lifecycle.State.STARTED) {
-                customerViewModel.getPestLog(1, memberId)
-                customerViewModel.detectionHistoryResponse.collect() { res ->
-                    pestLogListRVAdapter.submitList(res.body()?.detectionHistoryDtoList)
+                mainViewModel.getBugRecordList(memberId)
+                mainViewModel.detectionHistory.collect() { res ->
+                    pestLogListRVAdapter.submitList(res.body())
                 }
             }
         }
     }
 
     override fun onItemClick(item: Any) {
-        TODO("Not yet implemented")
+        if (item is DetectionHistory) {
+            val args = Bundle().apply {
+                item.id?.let { putLong("detectionHistoryId", it) }
+            }
+            val action = PestLogListFragmentDirections.actionPestLogListFragmentToPestLogFragment()
+            findNavController().navigateSafe(action.actionId, args)
+        }
     }
 
 }
